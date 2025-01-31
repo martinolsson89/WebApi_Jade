571b8d19fe3689dad6694d00da1610822b2e29d5
Martin Olsson <martinolsson89@gmail.com>
2025-01-31 14:17:13 +0100

    rebuild db

--- DbContext/Migrations/SqlServerDbContext/20250131130935_miInitial.Designer.cs
+++ DbContext/Migrations/SqlServerDbContext/20250131130935_miInitial.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DbContext.Migrations.SqlServerDbContext
 {
     [DbContext(typeof(MainDbContext.SqlServerDbContext))]
-    [Migration("20250130095917_miInitial")]
+    [Migration("20250131130935_miInitial")]
     partial class miInitial
     {
         /// <inheritdoc />
