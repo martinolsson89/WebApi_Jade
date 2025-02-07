76c2066f06a177b68b07610ab78e032aa4cd842d
Martin Olsson <martinolsson89@gmail.com>
2025-02-07 11:09:52 +0100

    fixed comment to be included in readitems (Attractions)

--- DbContext/Migrations/SqlServerDbContext/20250207095106_miInitial.Designer.cs
+++ DbContext/Migrations/SqlServerDbContext/20250207095106_miInitial.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DbContext.Migrations.SqlServerDbContext
 {
     [DbContext(typeof(MainDbContext.SqlServerDbContext))]
-    [Migration("20250206122910_miInitial")]
+    [Migration("20250207095106_miInitial")]
     partial class miInitial
     {
         /// <inheritdoc />
