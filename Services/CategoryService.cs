86bf1d59a31ed324736a1b92f798650720184328
Steffebresse <143505834+Steffebresse@users.noreply.github.com>
2025-02-11 11:06:59 +0100

    update

--- Services/CategoryService.cs
+++ Services/CategoryService.cs
@@ -3,12 +3,12 @@ using Microsoft.Extensions.Logging;
 using Models;
 using Models.DTO;
 
-namespace Services;
+namespace Services;     
 
-public class CategoryServiceDb : ICategoryService
+public class CategoryServiceDb : ICategoryService    
 {
-    private readonly CategoryDbRepos _categoryDbRepos;
-    private readonly ILogger<CategoryServiceDb> _logger;    
+    private readonly CategoryDbRepos _categoryDbRepos; 
+    private readonly ILogger<CategoryServiceDb> _logger;  
     
     public CategoryServiceDb(CategoryDbRepos categoryDbRepos, ILogger<CategoryServiceDb> logger)
     {
@@ -18,7 +18,6 @@ public class CategoryServiceDb : ICategoryService
 
     public Task<ResponseItemDto<ICategory>> ReadCategoryAsync(Guid id, bool flat) => _categoryDbRepos.ReadItemAsync(id, flat);
     
-
     public Task<ResponsePageDto<ICategory>> ReadCategoriesAsync(bool seeded, bool flat, string filter, int pageNumber, int pageSize) => _categoryDbRepos.ReadItemsAsync(seeded, flat, filter, pageNumber, pageSize);
 
     public Task<ResponseItemDto<ICategory>> DeleteCategoryAsync(Guid id) => _categoryDbRepos.DeleteItemAsync(id);
@@ -26,4 +25,5 @@ public class CategoryServiceDb : ICategoryService
     public Task<ResponseItemDto<ICategory>> PostCategoryAsync(CategoryCuDto item) => _categoryDbRepos.CreateItemAsync(item);
 
     public Task<ResponseItemDto<ICategory>> UpdateCategoryAsync(CategoryCuDto item) => _categoryDbRepos.UpdateItemAsync(item);
-    }
+
+}
