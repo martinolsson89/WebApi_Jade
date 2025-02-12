8fa57838b3831266d9c0dafd25ec2027c2504be9
Martin Olsson <martinolsson89@gmail.com>
2025-02-12 11:53:02 +0100

    updated clearDatabaseAzure script

--- DbContext/SqlScripts/clearDatabaseAzure.sql
+++ DbContext/SqlScripts/clearDatabaseAzure.sql
@@ -6,18 +6,18 @@ DROP PROCEDURE IF EXISTS gstusr.spLogin
 GO
 
 -- remove roles
-ALTER ROLE zoosefcGstUsr DROP MEMBER gstusrUser;
-ALTER ROLE zoosefcGstUsr DROP MEMBER usrUser;
-ALTER ROLE zoosefcGstUsr DROP MEMBER supusrUser;
+ALTER ROLE jadedbGstUsr DROP MEMBER gstusrUser;
+ALTER ROLE jadedbGstUsr DROP MEMBER usrUser;
+ALTER ROLE jadedbGstUsr DROP MEMBER supusrUser;
 
-ALTER ROLE zoosefcUsr DROP MEMBER usrUser;
-ALTER ROLE zoosefcUsr DROP MEMBER supusrUser;
+ALTER ROLE jadedbUsr DROP MEMBER usrUser;
+ALTER ROLE jadedbUsr DROP MEMBER supusrUser;
 
-ALTER ROLE zoosefcSupUsr DROP MEMBER supusrUser;
+ALTER ROLE jadedbSupUsr DROP MEMBER supusrUser;
 
-DROP ROLE IF EXISTS zoosefcGstUsr;
-DROP ROLE IF EXISTS zoosefcUsr;
-DROP ROLE IF EXISTS zoosefcSupUsr;
+DROP ROLE IF EXISTS jadedbGstUsr;
+DROP ROLE IF EXISTS jadedbUsr;
+DROP ROLE IF EXISTS jadedbSupUsr;
 GO
 
 --drop users
@@ -31,20 +31,29 @@ DROP PROCEDURE IF EXISTS supusr.spDeleteAll
 GO
 
 DROP VIEW IF EXISTS [gstusr].[vwInfoDb]
-DROP VIEW IF EXISTS [gstusr].[vwInfoZoos]
-DROP VIEW IF EXISTS [gstusr].[vwInfoAnimals]
-DROP VIEW IF EXISTS [gstusr].[vwInfoEmployees]
+DROP VIEW IF EXISTS [gstusr].[vwInfoAttractions]
+DROP VIEW IF EXISTS [gstusr].[vwInfoCategories]
 GO
 
 --drop tables in the right order not to get fk conflicts
-DROP TABLE IF EXISTS supusr.TemplateModel;
+-- DROP TABLE IF EXISTS supusr.TemplateModel;
+-- DROP TABLE IF EXISTS dbo.Users;
+-- DROP TABLE IF EXISTS supusr.EmployeeDbMZooDbM;
+-- DROP TABLE IF EXISTS supusr.Animals;
+-- DROP TABLE IF EXISTS supusr.Zoos;
+-- DROP TABLE IF EXISTS supusr.CreditCards;
+-- DROP TABLE IF EXISTS supusr.Employees;
+-- DROP TABLE IF EXISTS dbo.__EFMigrationsHistory;
+
+DROP TABLE IF EXISTS supusr.Comments;
+DROP TABLE IF EXISTS supusr.Attractions
+DROP TABLE IF EXISTS supusr.Addresses;
+DROP TABLE IF EXISTS supusr.Categories;
+DROP TABLE IF EXISTS supusr.MusicGroups;
 DROP TABLE IF EXISTS dbo.Users;
-DROP TABLE IF EXISTS supusr.EmployeeDbMZooDbM;
-DROP TABLE IF EXISTS supusr.Animals;
-DROP TABLE IF EXISTS supusr.Zoos;
-DROP TABLE IF EXISTS supusr.CreditCards;
-DROP TABLE IF EXISTS supusr.Employees;
+DROP TABLE IF EXISTS dbo.RoleDbM;
 DROP TABLE IF EXISTS dbo.__EFMigrationsHistory;
+
 GO
 
 -- drop schemas
